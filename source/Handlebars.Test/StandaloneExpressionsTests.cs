ac34b9a53f788be1912869c5d55bc05c3e41edeb
Alex Willis <alexmwillis@outlook.com>
2016-01-25 17:16:58 +0100

    remove whitespace around standalone expressions

---
+++ source/Handlebars.Test/StandaloneExpressionsTests.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace HandlebarsDotNet.Test
+{
+    [TestFixture]
+    public class StandaloneExpressionsTests
+    {
+        [TestCase("a\r\n{{#with block}}\r\nb\r\n{{/with}}\r\nc")]
+        [TestCase("a\n{{#with block}}\nb\n{{/with}}\nc")]
+        [TestCase("a\r\n  {{#with block}}  \r\nb\r\n  {{/with}}  \r\nc")]
+        public void WhenHelperExpressionsIsOnOwnLine(string source)
+        {
+            var template = Handlebars.Compile(source);
+
+            var actual = template(new { block = "block" });
+            Assert.That(actual, Is.EqualTo("a\r\nb\r\nc"));
+        }
+
+        [TestCase("a\r\n{{#with block}}\r\nb{{/with}}  \r\nc", "a\r\nb  \r\nc")] 
+        [TestCase("a  {{#with block}}  \r\nb\r\n{{/ with}}  c", "a    \r\nb\r\n  c")] 
+        public void WhenHelperExpressionsIsNotOnOwnLine(string source, string expected)
+        {
+            var template = Handlebars.Compile(source);
+
+            var actual = template(new {block = "block"});
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void NonBlockHelpersAreIgnored()
+        {
+            var source = "a\n{{this}}\nc";
+            var template = Handlebars.Compile(source);
+
+            var actual = template("b");
+            Assert.That(actual, Is.EqualTo("a\nb\nc"));
+        }
+
+        [Test]
+        public void NestedBlocks()
+        {
+            var source = "{{#with 'block'}}\r\n{{#with 'another block'}}\r\nsome content\r\n{{/with}}\r\n{{/with}}";
+            var template = Handlebars.Compile(source);
+
+            var actual = template(null);
+            Assert.That(actual, Is.EqualTo("some content\r\n"));
+        }
+
+        [Test]
+        public void DoubleNewLines()
+        {
+            var source = "a\r\n{{#with block}}\r\nb\r\n\r\n{{/with}}\r\nc";
+
+            var template = Handlebars.Compile(source);
+
+            var actual = template(new { block = "block"});
+            Assert.That(actual, Is.EqualTo("a\r\nb\r\n\r\nc"));
+        }
+    }
+}
--- source/Handlebars/Compiler/Lexer/Converter/WhitespaceRemover.cs
+++ source/Handlebars/Compiler/Lexer/Converter/WhitespaceRemover.cs
@@ -2,68 +2,116 @@
 using System.Collections.Generic;
 using System.Linq;
 using HandlebarsDotNet.Compiler.Lexer;
-using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 
 namespace HandlebarsDotNet.Compiler
 {
     internal class WhitespaceRemover : TokenConverter
     {
-        public static IEnumerable<object> Remove(IEnumerable<object> sequence)
+        private static readonly Regex StartsNewLineRegex = new Regex("^[^\\S\r\n]*\r?\n");
+        private static readonly Regex EndsNewLineRegex = new Regex("\r?\n[^\\S\r\n]*$");
+        private readonly HandlebarsConfiguration _configuration;
+
+        public static IEnumerable<object> Remove(IEnumerable<object> sequence, HandlebarsConfiguration configuration)
         {
-            return new WhitespaceRemover().ConvertTokens(sequence).ToList();
+            return new WhitespaceRemover(configuration).ConvertTokens(sequence).ToList();
         }
 
-        private WhitespaceRemover()
+        private WhitespaceRemover(HandlebarsConfiguration configuration)
         {
+            _configuration = configuration;
         }
 
         public override IEnumerable<object> ConvertTokens(IEnumerable<object> sequence)
         {
+            var list = sequence.ToList();
+            FindStandaloneHelpers(list);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var prevEndToken = (i > 0 ? list[i - 1] : null) as EndExpressionToken;
+                var nextStartToken = (i < list.Count - 1 ? list[i + 1] : null) as StartExpressionToken;
+                var staticToken = list[i] as StaticToken;
+                if (staticToken == null)
+                {
+                    yield return list[i];
+                    continue;
+                }
+                if (prevEndToken != null)
+                {
+                    if (prevEndToken.TrimTrailingWhitespace)
+                    {
+                        staticToken = Token.Static(staticToken.Value.TrimStart());
+                    }
+                    if (prevEndToken.ExpressionIsStandalone)
+                    {
+                        staticToken = Token.Static(StartsNewLineRegex.Replace(staticToken.Value, ""));
+                    }
+                }
+                if (nextStartToken != null)
+                {
+                    if (nextStartToken.TrimPreceedingWhitespace)
+                    {
+                        staticToken = Token.Static(staticToken.Value.TrimEnd());
+                    }
+                    if (nextStartToken.ExpressionIsStandalone)
+                    {
+                        staticToken =
+                            Token.Static(EndsNewLineRegex.Replace(staticToken.Value, Environment.NewLine));
+                    }
+                }
+                yield return staticToken;
+            }
+        }
+
+        private void FindStandaloneHelpers(IEnumerable<object> sequence)
+        {
+            var staticTokenEndsOnNewLine = true;
             var enumerator = sequence.GetEnumerator();
             while (enumerator.MoveNext())
             {
                 var item = enumerator.Current;
-                if (item is EndExpressionToken && ((EndExpressionToken)item).TrimTrailingWhitespace)
+                if (item is StartExpressionToken && staticTokenEndsOnNewLine)
                 {
-                    var endToken = (EndExpressionToken)item;
-                    yield return endToken;
-                    item = GetNext(enumerator);
-                    if (item != null)
+                    var startExpressionToken = (StartExpressionToken) item;
+                    var isHelper = false;
+                    while (enumerator.MoveNext() && !(enumerator.Current is EndExpressionToken))
                     {
-                        if (item is StaticToken)
-                        {
-                            item = Token.Static(((StaticToken)item).Value.TrimStart());
-                        }
+                        var word = enumerator.Current as WordExpressionToken;
+                        isHelper = isHelper || (word != null && IsRegisteredHelperName(word.Value));
+                    }
+                    var endExpressionToken = enumerator.Current as EndExpressionToken;
+                    if (isHelper && endExpressionToken != null && enumerator.MoveNext())
+                    {
+                        var staticToken = enumerator.Current as StaticToken;
+                        var isStandalone = (staticToken != null) &&
+                                           StartsNewLineRegex.IsMatch(staticToken.Value);
+                        startExpressionToken.ExpressionIsStandalone = isStandalone;
+                        endExpressionToken.ExpressionIsStandalone = isStandalone;
+                        staticTokenEndsOnNewLine = (staticToken != null) && EndsNewLineRegex.IsMatch(staticToken.Value);
                     }
                 }
-                if (item is StaticToken)
+                else
                 {
-                    var staticToken = (StaticToken)item;
-                    item = GetNext(enumerator);
-                    if (item != null)
+                    if (item is StaticToken)
                     {
-                        if (item is StartExpressionToken && ((StartExpressionToken)item).TrimPreceedingWhitespace)
-                        {
-                            staticToken = Token.Static(staticToken.Value.TrimEnd());
-                        }
+                        staticTokenEndsOnNewLine = EndsNewLineRegex.IsMatch(((StaticToken) item).Value) ||
+                                                   string.IsNullOrEmpty(((StaticToken) item).Value);
                     }
-                    yield return staticToken;
-                    if (item == staticToken)
+                    else
                     {
-                        continue;
+                        staticTokenEndsOnNewLine = false;
                     }
-                }
-                if (item != null)
-                {
-                    yield return item;
                 }
             }
         }
 
-        private static object GetNext(IEnumerator<object> enumerator)
+        private bool IsRegisteredHelperName(string name)
         {
-            enumerator.MoveNext();
-            return enumerator.Current;
+            name = name.Replace("#", "").Replace("/","");
+            return _configuration.Helpers.ContainsKey(name)
+            || _configuration.BlockHelpers.ContainsKey(name)
+            || new []{ "else", "each" }.Contains(name);
         }
     }
 }
